96b282c176032c76990b69b7f72cfe956a71357d
SahilIjaz <hssahil2913@gmail.com>
2026-01-09 22:32:25 +0500

    function, lists, dictonary, loops

--- day_1.py
+++ day_1.py
@@ -102,3 +102,53 @@ def is_palindrome(string):
 string = input('Enter a string:')
 print(is_palindrome(string))
 
+
+print('Lists now')
+
+list_1=[1,2,3,4,5]
+
+print('List of numbers',list_1)
+
+lust_2=['apple','banana','cherry']
+
+print('List of fruits',lust_2)
+
+mixed_list=[1,'apple',3.14,True]
+print('Mixed list',mixed_list)
+
+print('Accessing elements in a list')
+print('First element of list_1:',list_1[0])
+print('Last element of lust_2:',lust_2[-1]) 
+
+print('Slicing list_1 from index 1 to 3:',list_1[1:4])
+print('Adding element to list_1')
+list_1.append(6)
+print('Updated list_1:',list_1)
+print('Removing element from lust_2')
+lust_2.remove('banana')
+print('Updated lust_2:',lust_2)
+
+
+
+for item in mixed_list:
+    print('Item:',item)
+
+
+print('Length of list_1:',len(list_1))
+print('Length of lust_2:',  len(lust_2))
+print('Length of mixed_list:',len(mixed_list))
+
+print(f"Hello,{name}")
+
+students = {
+
+     {
+         "name":'Sahil',
+         'age':20
+     }
+     ,
+     {
+         'name':'Ali',
+'age':25
+     }
+}
